3795a54618581063617a2464162664a3f8df849b
Mihail Gaberov <mihail.gaberov@gmail.com>
2017-06-21 23:13:11 +0200

    Connected drop target and show the highlighted overlay.

--- src/Board.js
+++ src/Board.js
@@ -6,31 +6,29 @@ import HTML5Backend from 'react-dnd-html5-backend'
 import Square from './Square'
 import Knight from './Knight'
 import { canMoveKnight, moveKnight } from './Game'
+import BoardSquare from './BoardSquare'
 
 
 class Board extends Component {
   renderSquare(i) {
     const x = i % 8
     const y = Math.floor(i / 8)
-    const black = (x + y) % 2 === 1
-
-    const [knightX, knightY] = this.props.knightPosition
-    const piece = (x === knightX && y === knightY) ?
-      <Knight /> :
-      null
-
     return (
       <div key={i}
-           style={{
-             width: '12.5%',
-             height: '12.5%'
-           }}
-           onClick={() => this.handleSquareClick(x, y)}>
-        <Square black={black}>
-          {piece}
-        </Square>
+           style={{ width: '12.5%', height: '12.5%' }}>
+        <BoardSquare x={x}
+                     y={y}>
+          {this.renderPiece(x, y)}
+        </BoardSquare>
       </div>
     )
+  }
+
+  renderPiece(x, y) {
+    const [knightX, knightY] = this.props.knightPosition
+    if (x === knightX && y === knightY) {
+      return <Knight />
+    }
   }
 
   render() {
--- src/BoardSquare.js
+++ src/BoardSquare.js
@@ -1,24 +1,58 @@
-/**
- * Created by Mihail on 6/21/2017.
- */
 import React, { Component } from 'react'
 import PropTypes from 'prop-types'
 import Square from './Square'
+import { canMoveKnight, moveKnight } from './Game'
+import { ItemTypes } from './Constants'
+import { DropTarget } from 'react-dnd'
 
-export default class BoardSquare extends Component {
+const squareTarget = {
+  drop(props) {
+    moveKnight(props.x, props.y)
+  }
+}
+
+function collect(connect, monitor) {
+  return {
+    connectDropTarget: connect.dropTarget(),
+    isOver: monitor.isOver()
+  }
+}
+
+class BoardSquare extends Component {
   render() {
-    const { x, y } = this.props
+    const { x, y, connectDropTarget, isOver } = this.props
     const black = (x + y) % 2 === 1
 
-    return (
-      <Square black={black}>
-        {this.props.children}
-      </Square>
+    return connectDropTarget(
+      <div style={{
+        position: 'relative',
+        width: '100%',
+        height: '100%'
+      }}>
+        <Square black={black}>
+          {this.props.children}
+        </Square>
+        {isOver &&
+        <div style={{
+          position: 'absolute',
+          top: 0,
+          left: 0,
+          height: '100%',
+          width: '100%',
+          zIndex: 1,
+          opacity: 0.5,
+          backgroundColor: 'yellow',
+        }} />
+        }
+      </div>
     )
   }
 }
 
 BoardSquare.propTypes = {
   x: PropTypes.number.isRequired,
-  y: PropTypes.number.isRequired
+  y: PropTypes.number.isRequired,
+  isOver: PropTypes.bool.isRequired
 }
+
+export default DropTarget(ItemTypes.KNIGHT, squareTarget, collect)(BoardSquare)
